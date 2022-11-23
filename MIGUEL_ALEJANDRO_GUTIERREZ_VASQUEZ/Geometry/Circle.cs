4c7437bfb6a3529187bfb946c1bc28c60a6fa9f5
Miguel Alejandro Gutiérrez Vásquez <116773429+MGPOO@users.noreply.github.com>
2022-11-23 08:26:05 -0500

    "Geometry MG"

---
+++ MIGUEL_ALEJANDRO_GUTIERREZ_VASQUEZ/Geometry/Circle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Geometry
+{
+    class Circle
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+        public double CalculateArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+        public double CalculatePerimeter()
+        {
+            return 2 * Math.PI * Radius;
+        }
+        double CalculateArea();
+        double CalculatePerimeter();
+        Rectangle rectangle = new Rectangle(6, 5);
+        Square square = new Square(10);
+        Circle circle = new Circle(7);
+
+        IShape[] shapes = { rectangle, square, circle };
+
+            foreach (IShape shape in shapes)
+            {
+                Console.WriteLine(shape.CalculateArea());
+                Console.WriteLine(shape.CalculatePerimeter());
+
+            }
+}
+}
---
+++ MIGUEL_ALEJANDRO_GUTIERREZ_VASQUEZ/Geometry/Square.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Geometry
+{
+    class Square
+    {
+        public Square(double length) : base(length, length)
+        {
+
+        }
+----------------------------------------------------
+double CalculateArea();
+        double CalculatePerimeter();
+--------------------------------------------------------
+Rectangle rectangle = new Rectangle(6, 5);
+        Square square = new Square(10);
+        Circle circle = new Circle(7);
+
+        IShape[] shapes = { rectangle, square, circle };
+
+            foreach (IShape shape in shapes)
+            {
+                Console.WriteLine(shape.CalculateArea());
+                Console.WriteLine(shape.CalculatePerimeter());
+
+            }
+}
+}
